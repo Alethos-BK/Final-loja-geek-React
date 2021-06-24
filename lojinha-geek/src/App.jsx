d1c118e222df179b76c8a738d233f8ef635ac9a5
LarissaCouto14 <81425436+LarissaCouto14@users.noreply.github.com>
2021-06-24 15:38:13 -0300

    alterações

--- lojinha-geek/src/App.jsx
+++ lojinha-geek/src/App.jsx
@@ -1,77 +1,78 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import Categoria from './produto/categoria/Categoria'
-
 import Home from "./Home"
-import Dropdown from 'react-bootstrap/Dropdown'
 import { Button, Navbar, NavDropdown, Container, Nav, FormControl, Form} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Produto from './produto/Produto';
-
 import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
-
+import Produto from './produto/Produto';
+// import Canecas from './Canecas';
+// import Roupas from './Roupas';
+// import Funkos from './Funkos';
 
 function App() {
 
   return (
-<>
-<BrowserRouter> 
-  <Navbar bg="light" expand="lg">
-    <Container>
-      <Navbar.Brand href="#home">LOJINHA</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-        <Nav.Link> <Link to="/Home">Inicio</Link></Nav.Link>
-          <Nav.Link> <Link to="/Produtos">Produtos</Link></Nav.Link>
-          
-
-          <NavDropdown title="Categoria" id="basic-nav-dropdown">
-
-            
-            <NavDropdown.Item href="#action/3.2">Jogos</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Acessórios</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Form inline>
-        <FormControl type="text" placeholder="Pesquisar" className="mr-sm-2" />
-        <Button variant="outline-success">ENTER</Button>
-      </Form>
-    </Navbar.Collapse>
-    </Container>
-    </Navbar>
-    <Home />
-   
-
+    <>
+    <BrowserRouter> 
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">LOJINHA</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+            <Nav.Link> <Link to="/Home">Inicio</Link></Nav.Link>
+              <Nav.Link> <Link to="./produto/Produto">Produto</Link></Nav.Link>
+              
     
-    
-    
+              <NavDropdown title="Categoria" id="basic-nav-dropdown">
+                 <NavDropdown.Item> <Link to= "/Canecas">Canecas</Link></NavDropdown.Item>
+                <NavDropdown.Item><Link to= "/Roupas">Roupas</Link></NavDropdown.Item>
+                <NavDropdown.Item ><Link to="/Funkos">Funkos</Link></NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+            <FormControl type="text" placeholder="Pesquisar" className="mr-sm-2" />
+            <Button variant="outline-success">ENTER</Button>
+          </Form>
+        </Navbar.Collapse>
+        </Container>
+        </Navbar>
+        
+        
+        
         <Switch>
-          <Route path="/Home" exact>
-            <Home />
-            
-            
-          </Route>
-            
-          <Route path="/Produtos" exact>
-            <Produtos/>
+              <Route path="/Home" exact>
+                <Home />
             </Route>
-
-            <Route path="/Categoria" exact>
-            <Categoria/>
-
+                
+              <Route path="./produto/Produto" exact>
+                <Produto/>
+             </Route>
+    
+                <Route path="/Categoria" exact>
+                <Categoria/>
             </Route>
-          
-        </Switch>
-      </BrowserRouter>
-
-
-  </>
-  );
-
-}
-
-export default App;
+    
+                {/* <Route path="/Canecas" exact>
+                <Canecas/>
+            </Route>
+    
+            <Route path="/Roupas" exact>
+                <Roupas/>
+            </Route>
+    
+            <Route path="/Funkos" exact>
+                <Funkos/>
+            </Route>
+               */}
+       </Switch>
+          </BrowserRouter>
+    
+      </>
+      );
+    
+    }
+    
+    export default App;
+    
